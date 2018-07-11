0f0f62eb1674a14664960e2249794c76aac98d9d
Joanmanuel1 <40809044+Joanmanuel1@users.noreply.github.com>
2018-07-10 22:17:17 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+    var ancho   
+    var largo
+    var perimetro
 
+
+    ancho = parseInt(prompt("ingrese ancho"));
+    largo= parseInt(prompt("ingrese largo"));
+    perimetro= largo + ancho + largo + ancho;
+
+    {   
+alert(perimetro)
+    }   
 }

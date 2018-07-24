4730970948589624696e996727a06cecdf273631
Joanmanuel1 <40809044+Joanmanuel1@users.noreply.github.com>
2018-07-24 20:46:39 -0300

    Add files via upload

---
+++ 5-InstruccionWhile/5-InstruccionWhile/jsIteraciones (2).js
@@ -0,0 +1,10 @@
+function mostrar()
+{
+	alert('iteración while');
+
+var i = 10
+while(i<=1) {
+	console.log(i);
+	i = i - 1;
+}
+}//FIN DE LA FUNCIÓN
---
+++ 5-InstruccionWhile/5-InstruccionWhile/jsIteraciones (3).js
@@ -0,0 +1,13 @@
+function mostrar()
+{
+
+var clave = prompt("ingrese el número clave.");
+
+while(clave != "utn750" ){
+
+clave = prompt("Clave incorrecta. Ingrese el número clave.");
+
+}
+
+alert("Clave valida")
+}//FIN DE LA FUNCIÓN

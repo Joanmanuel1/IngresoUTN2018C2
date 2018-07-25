2ffd223c360af40b2f1f7e378eeb323b0303484d
Joanmanuel1 <40809044+Joanmanuel1@users.noreply.github.com>
2018-07-24 22:01:55 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,11 @@ function mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while(clave != "utn750" ){
 
+clave = prompt("Clave incorrecta. Ingrese el número clave.");
+
+}
+
+alert("Clave valida")
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,13 @@ function mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
+sexo=sexo.toLowerCase()
 
+while (sexo != "f" &&  sexo != "m" ) {
+	
+	sexo =(prompt("Error. Ingrese un sexo entre f y m."));
+    sexo=sexo.toLowerCase()
+}
 
-
-document.getElementById('Sexo').value=sexo;
-
+document.getElementById("Sexo").value = sexo;
 }//FIN DE LA FUNCIÓN

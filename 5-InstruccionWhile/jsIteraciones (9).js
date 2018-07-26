568161b40cb86c78e6c58098c36e36e1476945d9
Joanmanuel1 <40809044+Joanmanuel1@users.noreply.github.com>
2018-07-25 21:36:04 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -6,13 +6,33 @@ function mostrar()
 	
 	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
+
+	var numero;
+	var maximo;
+	var minimo;
+
+do{ 
+	numero =parseInt(prompt(" Ingrese un numero"));
+	contador++;
+
+
+if ( contador == 1){
+
+	maximo = numero;
+	minimo = numero;
+}
+if(numero >maximo){
+	maximo = numero;
+}
+if( numero < minimo){
+	minimo = numero;
+}
+respuesta = prompt("Desea continuar?");
+} while(respuesta == "si");
 	
-	}
-
-
+		
+document.getElementById("maximo").value = maximo;
+document.getElementById("minimo").value = minimo;
 
 
 }//FIN DE LA FUNCIÓN
